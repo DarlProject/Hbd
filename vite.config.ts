271a0ab9dc3a9bc4aad972a0b89fa72cb1f235ac
DarlProject <darrell.rassya@gmail.com>
2026-01-05 23:19:32 +0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: './',   // 🔥 INI WAJIB
+  plugins: [react()],
 })
