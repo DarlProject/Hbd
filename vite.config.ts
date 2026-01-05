5159692be730b582c8ea9d2aaf6149dc0b7b140e
DarlProject <darrell.rassya@gmail.com>
2026-01-06 00:05:23 +0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Hbd/',   // ⬅️ GANTI
+  base: '/Hbd/',   //
   plugins: [react()],
 })
